641b247e17f46108f63ed6cccf3e6b6e47e7ef87
DononFed <115092180+DononFed@users.noreply.github.com>
2024-03-23 16:04:44 +0300

    Our rush, our power

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this fucking comment is incorrect
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
